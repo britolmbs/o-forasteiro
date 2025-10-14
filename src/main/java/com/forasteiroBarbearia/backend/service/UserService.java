3fcfe03c18f9a3907e120f0a1eaf66b59d056dc6
Lucas Brito <britolmbs@hotmail.com>
2025-10-14 15:56:39 -0300

    Feat: Criação do UserService

--- src/main/java/com/forasteiroBarbearia/backend/service/UserService.java
+++ src/main/java/com/forasteiroBarbearia/backend/service/UserService.java
@@ -4,7 +4,11 @@ import com.forasteiroBarbearia.backend.dto.UserDto;
 import com.forasteiroBarbearia.backend.model.User;
 import com.forasteiroBarbearia.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class UserService {
@@ -16,5 +20,18 @@ public class UserService {
         if (userDto.getEmail() == null || userDto.getEmail().isEmpty()) {
             throw  new IllegalArgumentException("Email é requerido");
         }
+        Optional<User> existingUser = userRepository.findByEmail(userDto.getEmail());
+        if(existingUser.isPresent()) {
+            throw new IllegalArgumentException(("Usuario já contem email"));
+        }
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String hashedPassword = passwordEncoder.encode(userDto.getPassword());
+
+        User newUser = new User();
+        newUser.setId(UUID.randomUUID().toString());
+        newUser.setName(userDto.getEmail());
+        newUser.setPassword(hashedPassword);
+
+        return userRepository.save(newUser);
     }
 }
